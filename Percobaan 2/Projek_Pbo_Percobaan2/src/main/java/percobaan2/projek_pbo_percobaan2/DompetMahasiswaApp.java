cc64132a2cf9a25d680efe9453ab386dfefa03bb
Muhammad Ananta <91576986+anantaryaaa@users.noreply.github.com>
2023-11-25 10:22:29 +0700

    Update DompetMahasiswaApp.java
    mengganti resource fxml filenya

--- Percobaan 2/Projek_Pbo_Percobaan2/src/main/java/percobaan2/projek_pbo_percobaan2/DompetMahasiswaApp.java
+++ Percobaan 2/Projek_Pbo_Percobaan2/src/main/java/percobaan2/projek_pbo_percobaan2/DompetMahasiswaApp.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class DompetMahasiswaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DompetMahasiswaApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(DompetMahasiswaApp.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
