7ab4b1ce79cef1d834c0b8e43bb5c0de3acb4d24
Muhammad Ananta <91576986+anantaryaaa@users.noreply.github.com>
2023-12-03 20:27:28 +0700

    Delete Percobaan 2 directory

--- Percobaan 2/Projek_Pbo_Percobaan2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module percobaan2.projek_pbo_percobaan2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens percobaan2.projek_pbo_percobaan2 to javafx.fxml;
-    exports percobaan2.projek_pbo_percobaan2;
-}
